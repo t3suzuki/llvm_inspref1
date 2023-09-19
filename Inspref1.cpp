224ceacc3cf123a8fba9935b0973a5586a797372
Tomoya Suzuki <t3suzuki@ucsd.edu>
2023-09-19 04:47:10 +0000

    up

--- Inspref1.cpp
+++ Inspref1.cpp
@@ -16,7 +16,6 @@
 
 using namespace llvm;
 
-static cl::opt<std::string> fdo_file("input-file", cl::desc("Specify FDO file."), cl::value_desc("filename"));
 static cl::opt<int> disc("input-disc", cl::desc("Specify discriminator."), cl::value_desc("discriminator"));
 
 static cl::opt<bool> my_verbose("verbose", cl::desc("verbose flag"), cl::value_desc("verbose"));
@@ -33,27 +32,6 @@ namespace {
     void getAnalysisUsage(AnalysisUsage &AU) const override {
       AU.addRequired<LoopInfoWrapperPass>();
     }
-#if 0
-    bool doInitialization(Module &M) override {
-      if (fdo_file.empty()){
-	errs() << "[Error] input file is not specified!\n";
-	return false;
-      }
-      auto FS = vfs::getRealFileSystem();      
-      LLVMContext &Ctx = M.getContext();
-      ErrorOr<std::unique_ptr<SampleProfileReader>> ReaderOrErr = SampleProfileReader::create(fdo_file, Ctx, *FS);
-      if (std::error_code EC = ReaderOrErr.getError()) {
-	std::string Msg = "Could not open profile: " + EC.message();
-	Ctx.diagnose(DiagnosticInfoSampleProfile(fdo_file, Msg, DiagnosticSeverity::DS_Warning));
-	return false;
-      }
-      
-      Reader = std::move(ReaderOrErr.get());
-      Reader->read();
-      
-      return true;
-    }
-#endif
 
     Instruction* GetIncomingValue(Loop* L, llvm::Instruction* curPN) {  
       BasicBlock *H = L->getHeader();
@@ -101,126 +79,6 @@ namespace {
       return nullptr;
     }
 
-#if 0
-    virtual bool runOnFunction(Function &F) override {
-      if (Reader) {
-	const llvm::sampleprof::FunctionSamples* read_samples = Reader->getSamplesFor(F);
-	if (read_samples) {
-	  LoopInfo &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
-	  for (auto &BB : F) {
-	    bool isBBLoop = LI.getLoopFor(&BB);
-	    for (auto &I : BB) {
-	      if (const DILocation *Loc = I.getDebugLoc()){
-		if (const auto *samples = read_samples->findFunctionSamples(Loc)) {
-		  auto ret = samples->findCallTargetMapAt(FunctionSamples::getOffset(Loc),
-							  Loc->getBaseDiscriminator()*2);
-		  if (ret) {
-		    Use* OperandList0 = I.getOperandList();
-		    LoadInst *curLoad = dyn_cast<LoadInst>(&I);
-		    if (!curLoad) {
-		      for (Use* op0 = OperandList0; op0 < OperandList0 + I.getNumOperands(); op0++) {
-			curLoad = dyn_cast<LoadInst>(op0->get());
-			if (curLoad)
-			  break;
-		      }
-		    }
-
-		    if (curLoad) {
-		      errs() << "curLoad : ";
-		      curLoad->dump();
-		      PHINode *phi = nullptr;
-		      Use* OperandList = curLoad->getOperandList();
-		      Loop* curLoop = LI.getLoopFor(curLoad->getParent());
-		      for (Use* op = OperandList; op < OperandList + curLoad->getNumOperands(); op++) {
-			Instruction* insn = dyn_cast<Instruction>(op->get());
-			errs() << "dep_insn : ";
-			insn->dump();
-			if (dyn_cast<PHINode>(insn)) {
-			  phi = dyn_cast<PHINode>(insn);
-			}
-
-			
-			Use* OperandList2 = insn->getOperandList();
-			for (Use* op2 = OperandList2; op2 < OperandList2 + insn->getNumOperands(); op2++) {
-			  Instruction* insn2 = dyn_cast<Instruction>(op2->get());
-			  if (insn2) {
-			    insn2->dump();
-			    errs() << "isLoopInv: " << curLoop->isLoopInvariant(insn2) << "\n";
-			    if (curLoop->isLoopInvariant(insn2)) {
-			      Type *I32 = Type::getInt32Ty(F.getContext());
-			      Type *I64 = Type::getInt64Ty(F.getContext());
-			      Function *PrefetchFunc = Intrinsic::getDeclaration((curLoad->getFunction())->getParent(), Intrinsic::prefetch, (curLoad->getOperand(0))->getType());
-
-			      if (phi) {
-				OtherInstr = nullptr;
-				Instruction* IncInstr = GetIncomingValue(curLoop, phi);
-				errs() << "IncInsn : ";
-				IncInstr->dump();
-				if (IncInstr->getOpcode() == Instruction::GetElementPtr && IncInstr->getOperand(0) == phi){
-				  CmpInst* compareInstr = getCompareInstrGetElememntPtr(curLoop, IncInstr);
-				  errs() << "CmpInsn : ";
-				  compareInstr->dump();
-
-				  if (OtherInstr) {
-				    IRBuilder<> builder(OtherInstr->getNextNonDebugInstruction());
-				    /*
-				    BasicBlock *prefBB = BasicBlock::Create(F.getContext(), "prefBB", &F);
-				    BasicBlock *exitBB = BasicBlock::Create(F.getContext(), "exitBB", &F);
-				    builder.SetInsertPoint(prefBB);
-				    */
-				    for (int i=0; i<2; i++) {
-				      Value *added = builder.CreateGEP(I32, OtherInstr, ConstantInt::get(I32 ,i*4), "added");
-				      /*
-				      Value *cond = builder.CreateICmpSGT(added, added, "cond");
-
-				      BasicBlock *thenBB = BasicBlock::Create(F.getContext(), "thenBB", &F);
-				      BasicBlock *elseBB = BasicBlock::Create(F.getContext(), "elseBB", &F);
-				      builder.CreateCondBr(cond, thenBB, elseBB);
-				      builder.SetInsertPoint(thenBB);
-				      */
-				      Value* args3[] = {
-							added,
-							ConstantInt::get(I32 ,0),
-							ConstantInt::get(I32 ,3),
-							ConstantInt::get(I32 ,1)
-				      };
-				      auto aargs3 = ArrayRef<Value *>(args3, 4);
-				      builder.CreateCall(PrefetchFunc, aargs3);
-				      /*
-				      builder.SetInsertPoint(elseBB);
-				      Value* args4[] = {
-							added,
-							ConstantInt::get(I32 ,0),
-							ConstantInt::get(I32 ,3),
-							ConstantInt::get(I32 ,1)
-				      };
-				      auto aargs4 = ArrayRef<Value *>(args4, 4);
-				      builder.CreateCall(PrefetchFunc, aargs4);
-				      */
-				      
-				    }
-				  }
-				}
-			      }
-			    }
-			  }
-			}
-		      }
-		    }
-		  }
-		}
-	      }
-	    }
-	  }
-	}
-      }
-      return false;
-    }
-  };
-}
-
-#else
-
     virtual bool runOnFunction(Function &F) override {
       if (disc) {
 	//errs() << "disc " << disc << "\n";
@@ -235,94 +93,90 @@ namespace {
 	      }
 	      
 	      if (Loc->getBaseDiscriminator() == disc) {
-		if (1) {
-		  if (1) {
-		    Use* OperandList0 = I.getOperandList();
-		    LoadInst *curLoad = dyn_cast<LoadInst>(&I);
-		    I.dump();
-		    if (!curLoad) {
-		      for (Use* op0 = OperandList0; op0 < OperandList0 + I.getNumOperands(); op0++) {
-			curLoad = dyn_cast<LoadInst>(op0->get());
-			if (curLoad)
-			  break;
-		      }
-		    }
+		Use* OperandList0 = I.getOperandList();
+		LoadInst *curLoad = dyn_cast<LoadInst>(&I);
+		I.dump();
+		if (!curLoad) {
+		  for (Use* op0 = OperandList0; op0 < OperandList0 + I.getNumOperands(); op0++) {
+		    curLoad = dyn_cast<LoadInst>(op0->get());
+		    if (curLoad)
+		      break;
+		  }
+		}
 		    
-		    if (curLoad) {
-		      errs() << "curLoad : ";
-		      curLoad->dump();
-		      PHINode *phi = nullptr;
-		      Use* OperandList = curLoad->getOperandList();
-		      Loop* curLoop = LI.getLoopFor(curLoad->getParent());
-		      for (Use* op = OperandList; op < OperandList + curLoad->getNumOperands(); op++) {
-			Instruction* insn = dyn_cast<Instruction>(op->get());
-			errs() << "dep_insn : ";
-			insn->dump();
-			if (dyn_cast<PHINode>(insn)) {
-			  phi = dyn_cast<PHINode>(insn);
-			}
+		if (curLoad) {
+		  errs() << "curLoad : ";
+		  curLoad->dump();
+		  PHINode *phi = nullptr;
+		  Use* OperandList = curLoad->getOperandList();
+		  Loop* curLoop = LI.getLoopFor(curLoad->getParent());
+		  for (Use* op = OperandList; op < OperandList + curLoad->getNumOperands(); op++) {
+		    Instruction* insn = dyn_cast<Instruction>(op->get());
+		    errs() << "dep_insn : ";
+		    insn->dump();
+		    if (dyn_cast<PHINode>(insn)) {
+		      phi = dyn_cast<PHINode>(insn);
+		    }
 
 			
-			Use* OperandList2 = insn->getOperandList();
-			for (Use* op2 = OperandList2; op2 < OperandList2 + insn->getNumOperands(); op2++) {
-			  Instruction* insn2 = dyn_cast<Instruction>(op2->get());
-			  if (insn2) {
-			    insn2->dump();
-			    errs() << "isLoopInv: " << curLoop->isLoopInvariant(insn2) << "\n";
-			    if (curLoop->isLoopInvariant(insn2)) {
-			      Type *I32 = Type::getInt32Ty(F.getContext());
-			      Type *I64 = Type::getInt64Ty(F.getContext());
-			      Function *PrefetchFunc = Intrinsic::getDeclaration((curLoad->getFunction())->getParent(), Intrinsic::prefetch, (curLoad->getOperand(0))->getType());
+		    Use* OperandList2 = insn->getOperandList();
+		    for (Use* op2 = OperandList2; op2 < OperandList2 + insn->getNumOperands(); op2++) {
+		      Instruction* insn2 = dyn_cast<Instruction>(op2->get());
+		      if (insn2) {
+			insn2->dump();
+			errs() << "isLoopInv: " << curLoop->isLoopInvariant(insn2) << "\n";
+			if (curLoop->isLoopInvariant(insn2)) {
+			  Type *I32 = Type::getInt32Ty(F.getContext());
+			  Type *I64 = Type::getInt64Ty(F.getContext());
+			  Function *PrefetchFunc = Intrinsic::getDeclaration((curLoad->getFunction())->getParent(), Intrinsic::prefetch, (curLoad->getOperand(0))->getType());
 
-			      if (phi) {
-				OtherInstr = nullptr;
-				Instruction* IncInstr = GetIncomingValue(curLoop, phi);
-				errs() << "IncInsn : ";
-				IncInstr->dump();
-				if (IncInstr->getOpcode() == Instruction::GetElementPtr && IncInstr->getOperand(0) == phi){
-				  CmpInst* compareInstr = getCompareInstrGetElememntPtr(curLoop, IncInstr);
-				  errs() << "CmpInsn : ";
-				  compareInstr->dump();
+			  if (phi) {
+			    OtherInstr = nullptr;
+			    Instruction* IncInstr = GetIncomingValue(curLoop, phi);
+			    errs() << "IncInsn : ";
+			    IncInstr->dump();
+			    if (IncInstr->getOpcode() == Instruction::GetElementPtr && IncInstr->getOperand(0) == phi){
+			      CmpInst* compareInstr = getCompareInstrGetElememntPtr(curLoop, IncInstr);
+			      errs() << "CmpInsn : ";
+			      compareInstr->dump();
 
-				  if (OtherInstr) {
-				    IRBuilder<> builder(OtherInstr->getNextNonDebugInstruction());
-				    /*
-				    BasicBlock *prefBB = BasicBlock::Create(F.getContext(), "prefBB", &F);
-				    BasicBlock *exitBB = BasicBlock::Create(F.getContext(), "exitBB", &F);
-				    builder.SetInsertPoint(prefBB);
-				    */
-				    for (int i=0; i<2; i++) {
-				      Value *added = builder.CreateGEP(I32, OtherInstr, ConstantInt::get(I32 ,i*4), "added");
-				      /*
-				      Value *cond = builder.CreateICmpSGT(added, added, "cond");
+			      if (OtherInstr) {
+				IRBuilder<> builder(OtherInstr->getNextNonDebugInstruction());
+				/*
+				  BasicBlock *prefBB = BasicBlock::Create(F.getContext(), "prefBB", &F);
+				  BasicBlock *exitBB = BasicBlock::Create(F.getContext(), "exitBB", &F);
+				  builder.SetInsertPoint(prefBB);
+				*/
+				for (int i=0; i<2; i++) {
+				  Value *added = builder.CreateGEP(I32, OtherInstr, ConstantInt::get(I32 ,i*4), "added");
+				  /*
+				    Value *cond = builder.CreateICmpSGT(added, added, "cond");
 
-				      BasicBlock *thenBB = BasicBlock::Create(F.getContext(), "thenBB", &F);
-				      BasicBlock *elseBB = BasicBlock::Create(F.getContext(), "elseBB", &F);
-				      builder.CreateCondBr(cond, thenBB, elseBB);
-				      builder.SetInsertPoint(thenBB);
-				      */
-				      Value* args3[] = {
-							added,
-							ConstantInt::get(I32 ,0),
-							ConstantInt::get(I32 ,3),
-							ConstantInt::get(I32 ,1)
-				      };
-				      auto aargs3 = ArrayRef<Value *>(args3, 4);
-				      builder.CreateCall(PrefetchFunc, aargs3);
-				      /*
-				      builder.SetInsertPoint(elseBB);
-				      Value* args4[] = {
-							added,
-							ConstantInt::get(I32 ,0),
-							ConstantInt::get(I32 ,3),
-							ConstantInt::get(I32 ,1)
-				      };
-				      auto aargs4 = ArrayRef<Value *>(args4, 4);
-				      builder.CreateCall(PrefetchFunc, aargs4);
-				      */
+				    BasicBlock *thenBB = BasicBlock::Create(F.getContext(), "thenBB", &F);
+				    BasicBlock *elseBB = BasicBlock::Create(F.getContext(), "elseBB", &F);
+				    builder.CreateCondBr(cond, thenBB, elseBB);
+				    builder.SetInsertPoint(thenBB);
+				  */
+				  Value* args3[] = {
+						    added,
+						    ConstantInt::get(I32 ,0),
+						    ConstantInt::get(I32 ,3),
+						    ConstantInt::get(I32 ,1)
+				  };
+				  auto aargs3 = ArrayRef<Value *>(args3, 4);
+				  builder.CreateCall(PrefetchFunc, aargs3);
+				  /*
+				    builder.SetInsertPoint(elseBB);
+				    Value* args4[] = {
+				    added,
+				    ConstantInt::get(I32 ,0),
+				    ConstantInt::get(I32 ,3),
+				    ConstantInt::get(I32 ,1)
+				    };
+				    auto aargs4 = ArrayRef<Value *>(args4, 4);
+				    builder.CreateCall(PrefetchFunc, aargs4);
+				  */
 				      
-				    }
-				  }
 				}
 			      }
 			    }
@@ -343,7 +197,6 @@ namespace {
 }
 
 
-#endif
 char Inspref1Pass::ID = 0;
 
 #if 0
